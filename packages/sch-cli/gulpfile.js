7b7f3cb67d67320f347cc7ac050368784926009d
Max Englander <max.englander@gmail.com>
2017-05-04 06:14:33 +0000

    make flow an independent gulp task

--- packages/sch-cli/gulpfile.js
+++ packages/sch-cli/gulpfile.js
@@ -6,13 +6,17 @@ const fs = require('fs-extra');
 const gulp = require('gulp');
 const path = require('path');
 
-gulp.task('build', ['clean'], () => {
+gulp.task('build', ['clean', 'typecheck'], () => {
     return gulp.src(path.join(__dirname, 'src/**/*.js'))
-               .pipe(flow())
                .pipe(babel())
                .pipe(gulp.dest(path.join(__dirname, 'lib')));
 });
 
 gulp.task('clean', (done) => {
     fs.remove(path.join(__dirname, 'lib'), done);
+});
+
+gulp.task('typecheck', () => {
+    return gulp.src(path.join(__dirname, 'src/**/*.js'))
+               .pipe(flow({ killFlow: true }));
 });
