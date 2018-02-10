ac8c88f754fa24429ecad256f98a01ab0c161fab
Max Englander <max.englander@gmail.com>
2018-02-10 15:11:36 -0500

    write more tests for StandardStateBuilder

--- packages/sch-fsm/test/standard-state-builder.spec.js
+++ packages/sch-fsm/test/standard-state-builder.spec.js
@@ -3,26 +3,77 @@
  */
 
 import { expect } from 'chai';
+import sinon from 'sinon';
 
 import { IllegalStateError } from '../src/errors';
-import type { StateBuilder, StateIdRegistry } from '../src/types';
-import { StandardStateBuilder } from '../src/state';
+import type { State, StateBuilder, StateId, StateIdRegistry,
+    Transition, TransitionFactory } from '../src/types';
+import { StandardState, StandardStateBuilder } from '../src/state';
 import { TestContext, TestTransition } from './util';
 
 describe('StandardStateBuilder', function () {
     let contextClass: Class<TestContext> = TestContext,
         stateBuilder: StateBuilder<TestContext>,
-        transitionFactory = function(stateIdRegistry: StateIdRegistry) {
-            const someStateId = stateIdRegistry.get('some-state-id');
-
-            return context => someStateId;
-        };
+        stateId: StateId = 'some-state-id',
+        transition: Transition<TestContext> = sinon.stub(),
+        transitionFactory: TransitionFactory<TestContext>
+            = sinon.stub().returns(transition);
 
     beforeEach(function () {
         stateBuilder = new StandardStateBuilder(TestContext);
     });
 
     describe('#build', function () {
+        context('when all required components have been supplied', function() {
+            let state: State<TestContext>;
+
+            beforeEach(function() {
+                state = stateBuilder.id(stateId)
+                    .transition(transitionFactory)
+                    .build();
+            });
+
+            it('returns a state', function() {
+                expect(state).to.not.be.null;
+            });
+
+            describe('the state', function() {
+                describe('#id', function() {
+                    it('is equal to the state id value provided to the state builder', function() {
+                        expect(state.id()).to.equal(stateId);
+                    });
+                });
+
+                describe('#transition', function() {
+                    it('is equal to the transition created by the supplied transition factory', function() {
+                        expect(state.transition()).to.equal(transition);
+                    });
+                });
+
+                describe('#transitionsTo', function() {
+                    const stateNames: Array<string> = ['some-state-1', 'some-state-2'],
+                        stateIds: Array<StateId> = [];
+
+                    before(function() {
+                        transitionFactory.callsFake(function(stateIdRegistry: StateIdRegistry) {
+                            stateNames.forEach(name => {
+                                stateIds.push(stateIdRegistry.get(name));
+                            });
+
+                            return transition;
+                        });
+                    });
+
+                    it('returns all and only the states registered by the transition factory', function() {
+                        expect(state.transitionsTo()).to.have.lengthOf(stateIds.length);
+                        state.transitionsTo().forEach((stateId, i) => {
+                            expect(stateId).to.equal(stateIds[i]);
+                        });
+                    });
+                });
+            });
+        });
+
         context('when no id has been supplied', function() {
             beforeEach(function() {
                 stateBuilder.transition(transitionFactory);
@@ -31,6 +82,17 @@ describe('StandardStateBuilder', function () {
             it('throws an IllegalStateError', function() {
                 expect(() => stateBuilder.build())
                     .to.throw(IllegalStateError, 'Cannot build a state without an id');
+            });
+        });
+
+        context('when no transition has been supplied', function() {
+            beforeEach(function() {
+                stateBuilder.id(stateId);
+            });
+
+            it('throws an IllegalStateError', function() {
+                expect(() => stateBuilder.build())
+                    .to.throw(IllegalStateError, 'Cannot build a state without a transition');
             });
         });
     });
@@ -50,6 +112,10 @@ describe('StandardStateBuilder', function () {
     describe('#transition', function () {
         it('returns its instance', function() {
             expect(stateBuilder.transition(transitionFactory)).to.equal(stateBuilder);
+        });
+
+        it('invokes the supplied transition factory', function() {
+            expect(transitionFactory.called).to.be.true;
         });
     });
 });
